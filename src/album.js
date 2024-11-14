56fbc76d8c8f5e5dda89f73130134b388734752c
xenochromia <lwmisdepressed@gmail.com>
2024-11-15 00:12:00 +0800

    fixed

--- src/album.js
+++ src/album.js
@@ -1,7 +1,7 @@
 // @ts-ignore
-import * as THREE from '../node_modules/three/build/three.module.js';
+import * as THREE from 'three';
 // @ts-ignore
-import {GLTFLoader} from '../node_modules/three/examples/jsm/loaders/GLTFLoader.js';
+import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js';
 
 
 const modal = document.getElementById("modal");
---
+++ vite.config.js
@@ -0,0 +1,5 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    base: '/FED_S10259001_Wai-Meng_Assg1_website/', // Replace with your repo name
+})
