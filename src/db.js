d74e463094393d067adb233797699f726a4421ee
xenochromia <lwmisdepressed@gmail.com>
2024-11-15 20:41:19 +0800

    fixed navbar active & removed env

--- src/db.js
+++ src/db.js
@@ -1,15 +1,38 @@
 import * as sb from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 const communityButton = document.getElementById('communityButton')
-const apiURL ="https://vladaopkzpgutrrgqnmy.supabase.co"
-const apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZsYWRhb3BrenBndXRycmdxbm15Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE2NTc5MzYsImV4cCI6MjA0NzIzMzkzNn0.TbTBfwwpNPpmMXLtL6KAGLx4pf0OMS-z32D4HRJ9HHc"
+const apiURL = window.env.APIURL
+const apiKey = window.env.APIKEY
 
 const supa = sb.createClient(apiURL, apiKey)
-communityButton.addEventListener('click', uploadData)
+communityButton.addEventListener('click', uploadData())
 async function uploadData() {
-    const email = document.getElementById('email').value
+    const email = 'test@gmail.com'
+    const name = 'test'
 
     const { data, error } = await supa
-        .from('community').insert([{ email:email }]);
+        .from('communityMembers').insert([{
+            Name: name,
+            Email: email,
+         }])
+
+    if (error) {
+        return error
+    } else {
+        return data
+    }
+}
+
+async function fetchId(id) {
+    const { data, error } = await supa
+        .from('communityForm')
+        .select("*")
+        .eq('id', id);
     
+    if (error) {
+        return false;
+    } else {
+        return true;
+    }
+
 }
